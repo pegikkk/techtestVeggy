625164a34463c59bffc6b7642287ba168d6f51cb
pegikkk <37980196+pegikkk@users.noreply.github.com>
2023-07-30 18:04:17 +0700

    update

--- cypress/e2e/addcart.js
+++ cypress/e2e/addcart.js
@@ -1,20 +1,30 @@
 describe('', () => {
-
+  var phonenumber = 81916027805
+  var otpinput = 1234
 
   it('A customer adds an item to their cart.', () => {
     cy.visit('/')
     cy.viewport(390,844)
-    cy.get('.css-ycd3nx > .MuiBottomNavigationAction-label').click()
-    cy.get('#phone-number-input').type(81916027805,{force:true})
-    cy.get('#next-signup-login-button').click({force:true})
-    cy.get('.modal-body')
-    cy.get('#otp-input').type(1234)
-    cy.get('#submit-signup-login-button').click({force:true})
-    cy.get('[tabindex="4"]').should('have.text', 'PROFILE');
-    cy.get(':nth-child(1) > [style="opacity: 1; box-shadow: rgba(128, 128, 128, 0.2) 0px 0px 5px; border: 1px solid rgb(205, 205, 205); padding: 10px; cursor: pointer; display: flex; flex-direction: row; margin: 5px; border-radius: 5px;"] > [style="display: flex; flex-direction: column; justify-content: center; width: 100%; text-align: left;"] > [style="flex-direction: row; justify-content: space-between;"] > #outlet-name-option').click()
+    cy.login(phonenumber, otpinput)
+    cy.wait(5000)
+    cy.get('span:contains("KIRU-KIRU SHABU")').click()
+    cy.wait(5000)
     cy.get('#add-item-button').click()
-    cy.get('#add-to-cart-button').should('have.text', 'Add to Cart');
-    cy.get('#add-to-cart-button').click()
-    cy.get('#cart-icon').click()
+    cy.wait(3000)
+    cy.get('body').then(($parent) => {
+      if ($parent.find('p:contains("This item already in cart")').length > 0) {
+        cy.get('button:contains("Make Another")').should('have.text', 'Make Another')
+        cy.get('button:contains("Make Another")').click()
+        cy.wait(5000)
+        cy.get('#add-to-cart-button').click()
+        cy.wait(3000)
+        cy.get('[data-testid="CloseIcon"]').click()
+      } else {
+        cy.get('#add-to-cart-button').should('have.text', 'Add to Cart')
+        cy.get('#add-to-cart-button').click()
+      }
+      
+      cy.get('#cart-icon').click()
+    })
   })
 })
--- cypress/e2e/register.js
+++ cypress/e2e/register.js
@@ -1,31 +1,52 @@
 describe('', () => {
+  require('@4tw/cypress-drag-drop')
   var chance = require('chance').Chance();
   var random_number = chance.integer({ min: 80000000, max: 89999999 });
   var name = chance.name()
   var email = chance.email()
   var street = chance.street()
   var unit = chance.natural({ min: 1, max: 20 })
-  var nric = chance.integer({ min: 1, max: 100 })
+  var nric = chance.integer({ min: 1000, max: 9999 })
+  var outletsignup = "AMK Outlet"
+  var howdoyouknowus = "Instagram"
+  var choosenumber = "1"
+  var choosedropdown = "Test1"
 
   it('Registering of a new customer', () => {
     cy.visit('/')
     cy.viewport(390,844)
+    cy.wait(3000)
     cy.get('.css-ycd3nx').click()
+    cy.wait(2000)
     cy.get('.modal-content')
     cy.get('#phone-number-input').type(random_number)
+    cy.wait(3000)
     cy.get('#next-signup-login-button').click({force:true})
     cy.get('#login-register-modal')
+    cy.wait(3000)
     cy.get('#name-phone-input').type(name)
     cy.get('#email-input').type(email)
-    cy.get('.date > div')
+    cy.get('.date > div').click()
+    cy.get(':nth-child(3) > .datepicker-viewport > .datepicker-wheel > .datepicker-scroll > :nth-child(6)')
+    .trigger('mousedown', { which: 1, pageX: 600, pageY: 100 })
+    .trigger('mousemove', { which: 1, pageX: 600, pageY: 1000 })
+    .trigger('mouseup')
+    cy.wait(2000)
     cy.get('.datepicker-navbar > :nth-child(1)').click({force:true})
     cy.get(':nth-child(2) > .un-select-gender > .fa').click()
     cy.get('#street').type(street)
     cy.get('#unitNo').type(unit)
-    cy.get('#nric\(last4digits\)').type(nric)
-    cy.get(':nth-child(5) > .woocommerce-FormRow > .styles_select__vvgdy').click()
-
-
-
+    cy.get('input[name="nric(last4digits)"]').type(nric)
+    cy.get('select[name="outletsignup"]').select(outletsignup)
+    cy.get('select[name="howdoyouknowus"]').select(howdoyouknowus)
+    cy.get('select[name="choosenumber"]').select(choosenumber)
+    cy.get('select[name="choosedropdown"]').select(choosedropdown)
+    cy.get('#create-account-button').click()
+    cy.wait(2000)
+    cy.get('.modal-body')
+    cy.get('#continue-button').click()
+    cy.get('#otp-input').type(1234)
+    cy.get('#continue-button').click()
+    cy.get('[tabindex="4"] > .MuiBottomNavigationAction-label').should('have.text', 'PROFILE');
   })
 })
